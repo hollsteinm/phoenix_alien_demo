ff19c98187a0cba4f98aca0e225311f397179236
Martin Hollstein <Hollsteinm@hotmail.com>
2020-01-13 15:20:21 -0600

    add a function plug for work status

--- lib/alien_demo_web/controllers/admin/comments_controller.ex
+++ lib/alien_demo_web/controllers/admin/comments_controller.ex
@@ -1,7 +1,22 @@
 defmodule AlienDemoWeb.Admin.CommentsController do
   use AlienDemoWeb.Admin, :controller
 
+  plug :fetch_comments
+  plug :flash_comments_workload
+
   def index(conn, _params) do
     render(conn, "index.html")
+  end
+
+  defp fetch_comments(conn, _) do
+    assign(conn, :comments, [])
+  end
+
+  defp flash_comments_workload(conn, _) do
+    if length(conn.assigns[:comments]) == 0 do
+      conn |> put_flash(:info, "Congratulations, you have no work to do. Go find some aliens!")
+    else
+      conn
+    end
   end
 end
