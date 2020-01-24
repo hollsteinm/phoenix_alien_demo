bcc20c959c79f50b6f8fd175306dbf2469a34e2d
Martin Hollstein <Hollsteinm@hotmail.com>
2020-01-24 11:19:03 -0600

    added a full repo example

--- lib/alien_demo_web/controllers/saucers_controller.ex
+++ lib/alien_demo_web/controllers/saucers_controller.ex
@@ -4,34 +4,17 @@ defmodule AlienDemoWeb.SaucersController do
 
   action_fallback AlienDemoWeb.ControllerErrorFallback
 
-  @saucers [
-    %{
-      :id => 0,
-      :name => "disc",
-      :description => "classic saucer with anti gravity engines.",
-      :comment_ids => [
-        123,
-        124,
-        125
-      ],
-      :reported_datetime => DateTime.utc_now()
-    },
-    %{
-      :id => 1,
-      :name => "rectangle",
-      :description =>
-        "commonly mistaken as a stealth bomber, the rectangle is an intimidating saucer",
-      :comment_ids => [],
-      :reported_datetime => DateTime.utc_now()
-    }
-  ]
-
   def index(conn, _params) do
     render(conn, :index,
       saucers:
         Enum.map(
           Repo.all(Saucer),
-          fn %Saucer{title: name, content: description, submission_datetime: reported_datetime, id: id} ->
+          fn %Saucer{
+               title: name,
+               content: description,
+               submission_datetime: reported_datetime,
+               id: id
+             } ->
             %{
               :name => name,
               :description => description,
@@ -45,14 +28,24 @@ defmodule AlienDemoWeb.SaucersController do
   end
 
   def show(conn, %{"id" => saucer_id}) do
-    with {:ok, saucer} <- fetch_saucers(saucer_id) do
+    with {:ok,
+          %Saucer{
+            title: name,
+            content: description,
+            submission_datetime: reported_datetime,
+            id: id
+          }} <- fetch_saucers(saucer_id) do
       conn
-      |> render(:show, saucer: saucer)
+      |> render(:show,
+        saucer: %{
+          :name => name,
+          :description => description,
+          :reported_datetime => reported_datetime,
+          :id => id,
+          :comment_ids => []
+        }
+      )
     end
-  end
-
-  def new(conn, _params) do
-    render(conn, "new.html")
   end
 
   def create(conn, %{"name" => title, "description" => content}) do
@@ -78,16 +71,15 @@ defmodule AlienDemoWeb.SaucersController do
     end
   end
 
-  defp fetch_saucers() do
-    {:ok, @saucers}
-  end
-
   defp fetch_saucers(id) do
-    with [saucer | _tail] <-
-           Enum.filter(@saucers, fn %{:id => saucer_id} -> "#{saucer_id}" == id end) do
+    with {saucer_id, _text} <- Integer.parse(id), saucer <- Repo.get(Saucer, saucer_id) do
       {:ok, saucer}
     else
-      [] -> {:error, :not_found}
+      nil ->
+        {:error, :not_found}
+
+      :error ->
+        {:error, :bad_request}
     end
   end
 end
